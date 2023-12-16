9a42cde065183d764045213b2e47f586b3054ccc
Edinho <git@final04.com>
2023-12-16 11:19:13 -0300

    passing tests

--- pingo/__init__.py
+++ pingo/__init__.py
@@ -1,1 +1,32 @@
 # api
+from .board import ANALOG  # noqa
+from .board import IN  # noqa
+from .board import OUT  # noqa
+from .board import PWM  # noqa
+from .board import HIGH  # noqa
+from .board import LOW  # noqa
+from .board import ModeNotSuported  # noqa
+from .board import WrongPinMode  # noqa
+from .board import PwmOutputCapable  # noqa
+from .board import AnalogInputCapable  # noqa
+from .board import Board  # noqa
+from .board import PwmPin  # noqa
+from .board import AnalogPin  # noqa
+from .board import DigitalPin  # noqa
+from .board import GroundPin  # noqa
+from .board import Pin  # noqa
+from .board import VccPin  # noqa
+from . import parts  # noqa
+
+# boards
+from . import rpi  # noqa
+from . import ghost  # noqa
+from . import intel  # noqa
+from . import udoo  # noqa
+from . import pcduino  # noqa
+from . import arduino  # noqa
+from . import bbb  # noqa
+
+# resources
+from . import detect  # noqa
+from . import test  # noqa
